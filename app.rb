cca58d5020a22a40560f0be842b529713724c82a
Louise <louiselai88@gmail.com>
2014-05-05 11:17:03 +0100

    added views file for sinatra and made a Gemfile

--- app.rb
+++ app.rb
@@ -1,8 +1,5 @@
-require 'rubygems'
 require 'sinatra'
 
 get '/' do
-	"<div>
-		<img src='http://theuglyvolvo.files.wordpress.com/2013/10/costume-sushi.jpg'>
-	</div>"
+	erb :index
 end
---
+++ views/index.rb
@@ -0,0 +1,3 @@
+<div style='border: 5px dashed aqua'>
+	<img src='http://theuglyvolvo.files.wordpress.com/2013/10/costume-sushi.jpg'>
+</div>
