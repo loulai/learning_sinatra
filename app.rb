171d82d4dbe6db1539442baff28d05a0c8f22f46
Louise <louiselai88@gmail.com>
2014-05-05 12:03:09 +0100

    now has a second page, and visitor can be identified if they input their name

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'shotgun'
 
 get '/' do
-	# @name = ["Amigo", "Oscar", "Viking"].sample
-	@name = params[:name]
+	@name = ["Amigo", "Oscar", "Viking"].sample
+	erb :index
+end
+
+get '/hello' do
+	@visitor_name = params[:visitor]
 	erb :index
 end
